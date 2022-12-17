23c09c8dbdea52662f4176a4998c38c4442a0c3a
ShaneAdrianOpinion <120696891+ShaneAdrianOpinion@users.noreply.github.com>
2022-12-17 22:03:08 +0800

    Add FoodOrder main function

--- src/FoodOrdering/FoodOrderSAO.java
+++ src/FoodOrdering/FoodOrderSAO.java
@@ -1,10 +1,41 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class FoodOrderSAO {
+public class FoodOrderingSystem extends JFrame{
     private JPanel panel1;
-    private JCheckBox pizzaCheckBox;
-    private JRadioButton noneRadioButton;
-    private JButton orderButton;
+    private JCheckBox cPizza;
+    private JRadioButton rbNone;
+    private JButton btnOrder;
+    private JCheckBox cBurger;
+    private JCheckBox cFries;
+    private JCheckBox cSoftDrinks;
+    private JCheckBox cTea;
+    private JCheckBox cSundae;
+    private JRadioButton rb5;
+    private JRadioButton rb10;
+    private JRadioButton rb15;
+
+    private FoodOrderingSystem() {
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                orderFood();
+            }
+        });
+    }
+
+    public void orderFood() {
+    }
+
+    public static void main(String[] args) {
+        FoodOrderingSystem app = new FoodOrderingSystem();
+        app.setContentPane(app.panel1);
+        app.setSize(600, 500);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+        app.setTitle("Food Ordering System");
+    }
 }
