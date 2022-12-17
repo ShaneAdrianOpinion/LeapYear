7e7af6931bb5bccc244100c668febc0d2899703e
ShaneAdrianOpinion <120696891+ShaneAdrianOpinion@users.noreply.github.com>
2022-12-17 20:26:01 +0800

    Complete LeapYear

--- src/LeapYear/LeapYearCheck.java
+++ src/LeapYear/LeapYearCheck.java
@@ -1,11 +1,42 @@
 package LeapYear;
-
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LeapYearCheck extends JFrame{
     private JPanel panel1;
-    private JTextField textField1;
-    private JButton checkButton;
+    private JTextField tfYear;
+    private JButton btnCheckYear;
+
+    public LeapYearCheck() {
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                yearCheck();
+            }
+        });
+    }
+
+    private void yearCheck() {
+        double yearInput = Double.parseDouble(tfYear.getText());
+        boolean leap;
+        if (yearInput % 4 == 0) {
+            if (yearInput % 100 == 0) {
+                leap = yearInput % 400 == 0;
+            } else {
+                leap = true;
+            }
+        } else {
+            leap = false;
+        }
+
+        if (leap) {
+            JOptionPane.showMessageDialog(panel1, "Leap Year");
+        } else {
+            JOptionPane.showMessageDialog(panel1, "Not a Leap Year");
+        }
+        tfYear.setText("");
+    }
 
     public static void main(String[] args) {
         LeapYearCheck app = new LeapYearCheck();
