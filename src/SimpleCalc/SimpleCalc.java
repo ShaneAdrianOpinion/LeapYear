be4abccccb208bb413aa938bdc93a896846d0c51
ShaneAdrianOpinion <120696891+ShaneAdrianOpinion@users.noreply.github.com>
2022-12-17 23:37:42 +0800

    Add SimpleCalc main function

--- src/SimpleCalc/SimpleCalc.java
+++ src/SimpleCalc/SimpleCalc.java
@@ -1,10 +1,23 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SimpleCalc {
+public class SimpleCalc extends JFrame {
     private JPanel panel1;
-    private JTextField textField1;
-    private JComboBox comboBox1;
-    private JButton computeResultButton;
+    private JTextField tfNumber1;
+    private JComboBox cbOperations;
+    private JButton btnCompute;
+    private JTextField tfNumber2;
+    private JTextField lblResult;
+
+    public static void main(String[] args) {
+        SimpleCalc app = new SimpleCalc();
+        app.setContentPane(app.panel1);
+        app.setSize(600, 300);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+        app.setTitle("Simple Calculator");
+    }
 }
